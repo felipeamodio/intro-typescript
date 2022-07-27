7464336989cebeb4ea0ab802f0fb595564d1aa5b
Felipe Alves <felipeaamodio@gmail.com>
2022-07-26 21:47:23 -0300

    send email with typescript

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,6 @@
 import {Request, Response} from 'express';
+import EmailService from '../services/EmailService';
+
 const users = [
     {name: 'Felipe', email: 'felipe@email.com.br'}
 ]
@@ -6,5 +8,15 @@ const users = [
 export default {
     async index(req: Request, res: Response){
         return res.json(users);
+    },
+
+    async create(req: Request, res: Response){
+        const emailService = new EmailService();
+
+        emailService.sendMail(
+            {name: 'Felipe Alves', email: 'felipe@email.com.br'},
+            {subject: 'Bem-vindo ao sistema', body: 'Seja bem-vindo, Felipe...aqui você irá encontrar todas as informações que precisa sobre Typescript'},
+        );
+        return res.send();
     }
 };
